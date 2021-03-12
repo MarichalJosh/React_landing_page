91f2527d384d657c374675b06585e35c931e5c12
Joshua <marichaljoshua@gmail.com>
2021-03-12 03:19:15 +0000

    React landing page

---
+++ src/js/component/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = () => {
+	return (
+		<footer className="footer">
+			<div className="container">
+				<span className="text-white">
+					Copyright @ Your Website 2019
+				</span>
+			</div>
+		</footer>
+	);
+};
---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Jumbotron = () => {
+	return (
+		<div className="jumbotron jumbotron-fluid">
+			<h1 className="display-4">Hello, world!</h1>
+			<p className="lead">
+				Lorem Ipsum is simply dummy text of the printing and typesetting
+				industry. Lorem Ipsum has been the industrys standard dummy text
+				ever since the 1500s, when an unknown printer took a galley of
+				type and scrambled it to make a type specimen book. It has
+				survived not only five centuries.
+			</p>
+			<a className="btn btn-primary btn-lg" href="#" role="button">
+				Call to Action!
+			</a>
+		</div>
+	);
+};
